2550ece7b48d6d5a2a47cba27105aaa061205f85
zhulien <zhulienivanov@yahoo.com>
2017-01-26 17:34:38 +0200

    Add implementations for Property and StreetProperty.

---
+++ Monopoly/Monopoly.Models/Implementations/Properties/Property.cs
@@ -0,0 +1,56 @@
+﻿using Monopoly.Models.Contracts.Properties;
+
+namespace Monopoly.Models.Implementations.Properties
+{
+    public abstract class Property : IProperty
+    {
+        private string name;
+        private int price;
+        private int mortgageValue;
+        private IPlayer owner;
+        private bool isMortgaged;
+
+        protected Property(string name, int price, int mortgageValue)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.MortgageValue = mortgageValue;
+
+            //this.owner = Bank;
+            this.IsMortgaged = false;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Price
+        {
+            get { return this.price; }
+            set { this.price = value; }
+        }
+
+        public int MortgageValue
+        {
+            get { return this.mortgageValue; }
+            set { this.mortgageValue = value; }
+        }
+
+        public IPlayer Owner
+        {
+            get { return this.owner; }
+            set { this.owner = value; }
+        }
+
+
+        public bool IsMortgaged
+        {
+            get { return this.isMortgaged; }
+            set { this.isMortgaged = value; }
+        }
+
+        public abstract int GetRentValue();
+    }
+}
---
+++ Monopoly/Monopoly.Models/Implementations/Properties/StreetProperty.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Monopoly.Models.Contracts.Properties;
+
+namespace Monopoly.Models.Implementations.Properties
+{
+    public class StreetProperty : Property, IStreetProperty
+    {
+        private int rentPrice;
+        private IStreetGroup streetGroup;
+        private int oneHouseRentValue;
+        private int twoHousesRentValue;
+        private int threeHousesRentValue;
+        private int fourHousesRentValue;
+        private int hotelRentValue;
+        private int houses;
+        private int hotels;
+
+        public StreetProperty(string name, int price, int mortgageValue, int rentPrice, IStreetGroup streetGroup, int oneHouseRentValue, int twoHousesRentValue, int threeHousesRentValue, int fourHousesRentValue, int hotelRentValue) : base(name, price, mortgageValue)
+        {
+            this.RentPrice = rentPrice;
+            this.StreetGroup = streetGroup;
+            this.OneHouseRentValue = oneHouseRentValue;
+            this.TwoHousesRentValue = twoHousesRentValue;
+            this.ThreeHousesRentValue = threeHousesRentValue;
+            this.FourHousesRentValue = fourHousesRentValue;
+            this.HotelRentValue = hotelRentValue;
+
+            this.Houses = 0;
+            this.Hotels = 0;
+        }
+
+        public int RentPrice
+        {
+            get { return this.rentPrice; }
+            set { this.rentPrice = value; }
+        }
+
+        public IStreetGroup StreetGroup
+        {
+            get { return this.streetGroup; }
+            set { this.streetGroup = value; }
+        }
+
+        public int OneHouseRentValue
+        {
+            get { return this.oneHouseRentValue; }
+            set { this.oneHouseRentValue = value; }
+        }
+
+        public int TwoHousesRentValue
+        {
+            get { return this.twoHousesRentValue; }
+            set { this.twoHousesRentValue = value; }
+        }
+
+        public int ThreeHousesRentValue
+        {
+            get { return this.threeHousesRentValue; }
+            set { this.threeHousesRentValue = value; }
+        }
+
+        public int FourHousesRentValue
+        {
+            get { return this.fourHousesRentValue; }
+            set { this.fourHousesRentValue = value; }
+        }
+
+        public int HotelRentValue
+        {
+            get { return this.hotelRentValue; }
+            set { this.hotelRentValue = value; }
+        }
+
+        public int Houses
+        {
+            get { return this.houses; }
+            set { this.houses = value; }
+        }
+
+        public int Hotels
+        {
+            get { return this.hotels; }
+            set { this.hotels = value; }
+        }
+
+        public override int GetRentValue()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
