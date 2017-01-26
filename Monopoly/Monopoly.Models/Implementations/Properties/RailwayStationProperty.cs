260bc7443fff697a5986777e3ff9a157c9d5e399
zhulien <zhulienivanov@yahoo.com>
2017-01-26 17:47:45 +0200

    Add implementation of the RailwayStationProperty class.

---
+++ Monopoly/Monopoly.Models/Implementations/Properties/RailwayStationProperty.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Monopoly.Models.Contracts.Properties;
+
+namespace Monopoly.Models.Implementations.Properties
+{
+    public class RailwayStationProperty : Property, IRailwayStationProperty
+    {
+        private int rentPrice;
+
+        public RailwayStationProperty(string name, int price, int mortgageValue, int rentPrice) : base(name, price, mortgageValue)
+        {
+            this.RentPrice = rentPrice;
+        }
+
+        public int RentPrice
+        {
+            get { return this.rentPrice; }
+            set { this.rentPrice = value; }
+        }
+
+        public override int GetRentValue()
+        {
+            // Check Railway properties count of owner
+            // switch by value (1, 2, 3, 4)
+
+            throw new NotImplementedException();
+        }
+    }
+}
