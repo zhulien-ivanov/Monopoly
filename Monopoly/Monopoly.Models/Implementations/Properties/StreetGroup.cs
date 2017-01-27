74f8ed888b7f55ed57b1f88b0bbfccc8d6aae92a
zhulien <zhulienivanov@yahoo.com>
2017-01-27 15:06:29 +0200

    Add StreetGroup implementation.

---
+++ Monopoly/Monopoly.Models/Implementations/Properties/StreetGroup.cs
@@ -0,0 +1,37 @@
+﻿using Monopoly.Models.Contracts.Properties;
+using Monopoly.Models.Enumerations;
+
+namespace Monopoly.Models.Implementations.Properties
+{
+    public class StreetGroup : IStreetGroup
+    {
+        private StreetColour streetColour;
+        private int housePrice;
+        private int hotelPrice;
+
+        public StreetGroup(StreetColour streetColour, int housePrice, int hotelPrice)
+        {
+            this.StreetColour = streetColour;
+            this.HousePrice = housePrice;
+            this.HotelPrice = hotelPrice;
+        }
+
+        public StreetColour StreetColour
+        {
+            get { return this.streetColour; }
+            set { this.streetColour = value; }
+        }
+
+        public int HousePrice
+        {
+            get { return this.housePrice; }
+            set { this.housePrice = value; }
+        }
+
+        public int HotelPrice
+        {
+            get { return this.hotelPrice; }
+            set { this.hotelPrice = value; }
+        }
+    }
+}
