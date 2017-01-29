88e3ed87a2f0f6725444db4a7974f1f8f9fe72aa
zhulien <zhulienivanov@yahoo.com>
2017-01-29 02:12:04 +0200

    Add IGameContext inferface and implementation.

---
+++ Monopoly/Monopoly.Models/Contracts/Contexts/IGameContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+using Monopoly.Models.Contracts.Players;
+using Monopoly.Models.Contracts.Properties;
+
+using Monopoly.Models.Helpers;
+
+namespace Monopoly.Models.Contracts.Contexts
+{
+    public interface IGameContext
+    {
+        IBank Bank { get; }
+
+        List<IMapObject> MapObjects { get; }
+
+        List<IPlayer> Players { get; }
+
+        IDictionary<IPlayer, int> PlayerPositions { get; }
+
+        IDictionary<IPlayer, PlayerStatusInformation> PlayerStatuses { get; }
+
+        IPlayer CurrentPlayer { get; }
+    }
+}
---
+++ Monopoly/Monopoly.Models/Implementations/Contexts/GameContext.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+
+using Monopoly.Models.Contracts.Contexts;
+using Monopoly.Models.Contracts.Players;
+using Monopoly.Models.Contracts.Properties;
+
+using Monopoly.Models.Helpers;
+
+namespace Monopoly.Models.Implementations.Contexts
+{
+    public class GameContext : IGameContext
+    {
+        private IBank bank;
+        private List<IMapObject> mapObjects;
+        private List<IPlayer> players;
+        IDictionary<IPlayer, int> playerPositions;
+        IDictionary<IPlayer, PlayerStatusInformation> playerStatuses;
+        private IPlayer currentPlayer;
+
+        public GameContext(IBank bank, List<IMapObject> mapObjects, List<IPlayer> players, IDictionary<IPlayer, int> playerPositions, IDictionary<IPlayer, PlayerStatusInformation> playerStatuses, IPlayer currentPlayer)
+        {
+            this.Bank = bank;
+            this.MapObjects = mapObjects;
+            this.Players = players;
+            this.PlayerPositions = playerPositions;
+            this.PlayerStatuses = playerStatuses;
+            this.CurrentPlayer = currentPlayer;
+        }
+
+        public IBank Bank
+        {
+            get { return this.bank; }
+            private set { this.bank = value; }
+        }
+
+        public List<IMapObject> MapObjects
+        {
+            get { return this.mapObjects; }
+            private set { this.mapObjects = value; }
+        }
+
+        public List<IPlayer> Players
+        {
+            get { return this.players; }
+            private set { this.players = value; }
+        }
+
+        public IDictionary<IPlayer, int> PlayerPositions
+        {
+            get { return this.playerPositions; }
+            private set { this.playerPositions = value; }
+        }
+
+        public IDictionary<IPlayer, PlayerStatusInformation> PlayerStatuses
+        {
+            get { return this.playerStatuses; }
+            private set { this.playerStatuses = value; }
+        }
+
+        public IPlayer CurrentPlayer
+        {
+            get { return this.currentPlayer; }
+            set { this.currentPlayer = value; }
+        }
+    }
+}
